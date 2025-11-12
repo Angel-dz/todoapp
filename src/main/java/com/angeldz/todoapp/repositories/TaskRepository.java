521a0c84b80e8fc363aef4ef4cfdbe18fdea51bd
Angel Dz <angel.dzaya@gmail.com>
2025-11-12 18:09:55 +0100

    adding two queries

--- src/main/java/com/angeldz/todoapp/repositories/TaskRepository.java
+++ src/main/java/com/angeldz/todoapp/repositories/TaskRepository.java
@@ -1,7 +1,16 @@
 package com.angeldz.todoapp.repositories;
 
 import com.angeldz.todoapp.model.Task;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TaskRepository  extends CrudRepository<Task,Long> {
+
+    @Query("SELECT * FROM task")
+    List<Task> findAllTasks();
+
+    @Query("SELECT * FROM task WHERE id = :id")
+    List<Task> findTasksById(long Id);
 }
